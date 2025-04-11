24b5415246c185c3f4d1bfa9ce6333b838f83938
AuroreBe <aurore.besson@univ-rennes1.fr>
2025-04-11 22:30:53 +0200

    method3 improved to work with liftoff on multiple species + log output

---
+++ bin/perform_liftoff_byPair.sh
@@ -0,0 +1,179 @@
+#!/bin/bash
+
+##########################################################################
+# Perform sequence alignment using liftoff and bedtools 
+# Developed by: A. Besson
+# Updated by : 
+##########################################################################
+
+PROGNAME=$(basename $0)
+
+# Function to display usage
+usage() {
+    echo "Usage: $PROGNAME --query_sp <QUERY_SPECIES> --query_fa <QUERY_FA> --query_gtf <QUERY_GTF> --target_sp <TARGET_SPECIES> --target_fa <TARGET_FA> --target_gtf <TARGET_GTF> --workdir <WORKDIR> [options]"
+    echo "Options:"
+    echo " --biotype [mRNA|lncRNA]     Biotype to analyze (default: lncRNA)"
+    echo " --flank [0.0-1.0]               Flank fraction for liftoff (default: 0)"
+    echo " --coverage [0.0-1.0]            Coverage fraction for liftoff (default: 0.5)"
+    echo " --identity [0.0-1.0]            Sequence identity fraction for liftoff (default: 0)"
+    echo " --withDB [logical]              to speed up the process, use feature database if already created"
+    exit 1
+}
+
+
+###########################
+# VARIABLES
+###########################
+
+# Parse command line arguments
+while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+        --query_sp) QUERY_SP="$2"; shift ;;
+        --query_fa) QUERY_FA="$2"; shift ;;
+        --query_gtf) QUERY_GTF="$2"; shift ;;
+        --target_sp) TARGET_SP="$2"; shift ;;
+        --target_fa) TARGET_FA="$2"; shift ;;
+        --target_gtf) TARGET_GTF="$2"; shift ;;
+        --workdir) WORKDIR="$2"; shift ;;
+        --biotype) BIOTYPE="$2"; shift ;;
+        --flank) FLANK="$2"; shift ;;
+        --coverage) COVERAGE="$2"; shift ;;
+        --identity) IDENTITY="$2"; shift ;;
+        --withDB) DATABASE="$2"; shift ;;
+        *) usage ;;
+    esac
+    shift
+done
+
+# sub-directory for each pair analysis
+COMPDIR="$WORKDIR"/"$QUERY_SP"_to_"$TARGET_SP"_"$BIOTYPE"
+mkdir -p ${COMPDIR}
+
+# log file
+LOG_FILE="$COMPDIR/alignment.log"
+{
+
+    # Arguments used
+    echo "Query genome: $QUERY_FA"
+    echo "Query annotation: $QUERY_GTF"
+    echo "Target genome: $TARGET_FA"
+    echo "Target annotation: $TARGET_GTF"
+    echo "Path to results directory: $WORKDIR"
+    echo "Gene biotype to analyse: $BIOTYPE"
+    echo "Flank value for liftoff: $FLANK"
+    echo "Coverage cut-off for liftoff: $COVERAGE"
+    echo "Sequence identity cut-off for liftoff: $IDENTITY"
+    echo "Features database available: $DATABASE"
+
+    ### STEP1 - liftoff
+
+    echo "Step1: liftoff analysis - $(date +"%d.%m.%Y %H:%M")"
+    echo "Working directory: $COMPDIR"
+
+    ## select features to add to `-f`
+    FEATURES=$COMPDIR/all_features.txt
+    grep -v '#' $QUERY_GTF | cut -f3 | sort | uniq > $FEATURES
+    echo "Features file created: $FEATURES"
+
+    ## create outfile and outdir
+    LIFTOFF_DIR="$COMPDIR"/liftoff_flank"$FLANK"
+    LO_OUT=liftoff_"$QUERY_SP"_to_"$TARGET_SP"_flank"$FLANK".gtf
+
+    ## launch liftoff script
+
+    ### source environment
+    . /local/env/envconda3.sh
+    conda activate activate /home/genouest/cnrs_umr6290/abesson/conda_env/liftoff_env
+
+    ### run liftoff
+    file_name=$(basename $QUERY_GTF .gtf)_"$BIOTYPE".gtf
+    file_path="$WORKDIR"/"$file_name"
+
+    if [ "$DATABASE" = FALSE ]; then
+        # subset annotation according biotype
+
+        echo "Feature database not existing. Creation of annotation file according biotype."
+        if [[ "$BIOTYPE" == "lncRNA" ]] ; then
+            # list of biotype string corresponding to lncRNA
+            lnc_list='"lncRNA"'
+            grep -E "gene_biotype ($(echo $lnc_list | sed 's/ /|/g'))" $QUERY_GTF > "$file_path"
+        elif [[ "$BIOTYPE" == "mRNA" ]] ; then
+            # list of biotype string corresponding to lncRNA
+            mrna_list='"mRNA" "protein_coding"'
+            grep -E "gene_biotype ($(echo $mrna_list | sed 's/ /|/g'))" $QUERY_GTF > "$file_path"
+        else
+            echo "Error: wrong biotype to analyse"
+        fi
+
+        echo "Subset annotation by biotype created: "$file_path""
+
+        echo "liftoff -g $file_path -o $LO_OUT -p 16 $TARGET_FA $QUERY_FA -flank $FLANK -f $FEATURES -dir $LIFTOFF_DIR -u $LIFTOFF_DIR/unmapped_features.txt -copies"
+        liftoff -g $file_path -o $LO_OUT -p 16 $TARGET_FA $QUERY_FA -flank $FLANK -f $FEATURES -dir $LIFTOFF_DIR -u $LIFTOFF_DIR/unmapped_features.txt -copies
+
+    elif [ "$DATABASE" = TRUE ]; then
+        QUERY_DB="$file_path"_db
+        echo "liftoff -db $QUERY_DB -o $LO_OUT -p 16 $TARGET_FA $QUERY_FA -flank $FLANK -f $FEATURES -dir $LIFTOFF_DIR -u $LIFTOFF_DIR/unmapped_features.txt -copies"
+        liftoff -db $QUERY_DB -o $LO_OUT -p 16 $TARGET_FA $QUERY_FA -flank $FLANK -f $FEATURES -dir $LIFTOFF_DIR -u $LIFTOFF_DIR/unmapped_features.txt -copies
+        
+    else
+        echo "Error for feature database (boolean value needed)"    
+    fi
+
+
+    # move features file in liftoff dir
+    mv $FEATURES $LIFTOFF_DIR
+    mv $LO_OUT $COMPDIR
+
+    LO_OUT2="$COMPDIR"/"$LO_OUT"
+
+    ### STEP2 - Filter liftoff results according cut-off arguments
+    echo "Step2: liftoff results filtering - $(date +"%d.%m.%Y %H:%M")"
+
+    # filtered file
+    LO_FILTER="$COMPDIR"/$(basename $LO_OUT2 .gtf)_filtered.gtf
+
+    conda deactivate
+    conda activate /home/genouest/cnrs_umr6290/abesson/conda_env/jupyterR_env
+
+    Rscript filter_liftoffOutput.R $LO_OUT2 $LO_FILTER $COVERAGE $IDENTITY $COMPDIR
+
+    ### STEP3 - Bedtools intersect
+    echo "Step3: bedtools analysis - $(date +"%d.%m.%Y %H:%M")"
+    BEDTOOLS_DIR="$COMPDIR"/bedtools_intersect
+    mkdir ${BEDTOOLS_DIR}
+
+    ### create outfile for liftoff output, target annotation and bedtools output
+    LO_BED="$BEDTOOLS_DIR"/$(basename $LO_FILTER .gtf).bed
+    TARGET_BED="$BEDTOOLS_DIR"/"$TARGET_SP".bed
+    FINAL_BED="$BEDTOOLS_DIR"/overlap_"$QUERY_SP"_to_"$TARGET_SP".bed
+
+    #### format gtf in bed 
+    . format_gtf2bed.sh $LO_FILTER > $LO_BED
+    echo "Bed file created: $LO_BED"
+
+    . format_gtf2bed.sh $TARGET_GTF > $TARGET_BED
+    echo "Bed file created: $TARGET_BED"
+
+    #### bedtools intersect - default fraction ~1bp
+    conda deactivate
+    . /local/env/envbedtools-2.27.1.sh
+
+    echo "Intersection analysis done by bedtools intersect."
+    bedtools intersect -a $LO_BED -b $TARGET_BED -wao -split > $FINAL_BED
+    echo "bedtools intersect -a $LO_BED -b $TARGET_BED -wao -split > $FINAL_BED"
+
+    ### STEP4 - Sequence alignment analysis
+    echo "Step4: Sequence alignment analysis - $(date +"%d.%m.%Y %H:%M")"
+    ALIGN_DIR="$COMPDIR"/alignment_analysis
+    mkdir ${ALIGN_DIR}
+
+    # load conda env => en créer un spécifiquement pour ça ??
+    conda deactivate
+    conda activate /home/genouest/cnrs_umr6290/abesson/conda_env/jupyterR_env
+
+    echo "Rscript seq_alignment_analysis.R $QUERY_GTF $TARGET_GTF $FINAL_BED $LIFTOFF_DIR/unmapped_features.txt $ALIGN_DIR"
+    Rscript seq_alignment_analysis.R $QUERY_GTF $TARGET_GTF $FINAL_BED $LIFTOFF_DIR/unmapped_features.txt $ALIGN_DIR
+
+    echo "Sequence alignment done: $(date +"%d.%m.%Y %H:%M")"
+
+} >> "$LOG_FILE" 2>&1
--- bin/step3_perform_seq_alignment.sh
+++ bin/step3_perform_seq_alignment.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --mem=34G
-#SBATCH --cpus-per-task=8
 #SBATCH --mail-user aurore.besson@univ-rennes.fr
 #SBATCH --mail-type=ALL
+#SBATCH --mem=34G
+#SBATCH --cpus-per-task=16
 
 ##########################################################################
 # Perform sequence alignment using liftoff and bedtools 
@@ -14,27 +14,23 @@ PROGNAME=$(basename $0)
 
 # Function to display usage
 usage() {
-    echo "Usage: $PROGNAME --query_fa <QUERY_FA> --query_gtf <QUERY_GTF> --target_fa <TARGET_FA> --target_gtf <TARGET_GTF> --workdir <WORKDIR> [options]"
+    echo "Usage: $PROGNAME --config <CONFIG> --workdir <WORKDIR> [options]"
     echo "Options:"
-    echo "  --biotype [mRNA|lncRNA|all]     Biotype to analyze (default: all)"
+    echo "  --biotype [mRNA|lncRNA]     Biotype to analyze (default: lncRNA)"
     echo "  --flank [0.0-1.0]               Flank fraction for liftoff (default: 0)"
-    echo " --coverage [0.0-1.0]             Coverage fraction for liftoff (default: 0.5)"
+    echo "  --coverage [0.0-1.0]            Coverage fraction for liftoff (default: 0.5)"
     echo "  --identity [0.0-1.0]            Sequence identity fraction for liftoff (default: 0)"
     exit 1
 }
-
 
 ###########################
 # VARIABLES
 ###########################
 
 # Initialize variables
-QUERY_FA=""
-QUERY_GTF=""
-TARGET_FA=""
-TARGET_GTF=""
+CONFIG=""
 WORKDIR=""
-BIOTYPE="all"
+BIOTYPE="lncRNA"
 FLANK=0
 COVERAGE=0.5
 IDENTITY=0
@@ -42,156 +38,115 @@ IDENTITY=0
 # Parse command line arguments
 while [[ $# -gt 0 ]]; do
     case "$1" in
-        --query_fa)
-            QUERY_FA="$2"
-            shift 2
-            ;;
-        --query_gtf)
-            QUERY_GTF="$2"
-            shift 2
-            ;;
-        --target_fa)
-            TARGET_FA="$2"
-            shift 2
-            ;;
-        --target_gtf)
-            TARGET_GTF="$2"
-            shift 2
-            ;;
-        --workdir)
-            WORKDIR="$2"
-            shift 2
-            ;;
-        --biotype)
-            BIOTYPE="$2"
-            shift 2
-            ;;
-        --flank)
-            FLANK="$2"
-            shift 2
-            ;;
-        --coverage)
-            COVERAGE="$2"
-            shift 2
-            ;;
-        --identity)
-            IDENTITY="$2"
-            shift 2
-            ;;
-        *)
-            usage
-            ;;
+        --config) CONFIG="$2"; shift 2 ;;
+        --workdir) WORKDIR="$2"; shift 2 ;;
+        --biotype) BIOTYPE="$2"; shift 2 ;;
+        --flank) FLANK="$2"; shift 2 ;;
+        --coverage) COVERAGE="$2"; shift 2 ;;
+        --identity) IDENTITY="$2"; shift 2 ;;
+        *) usage ;;
     esac
 done
 
 # Check if mandatory arguments are provided
-if [[ -z "$QUERY_FA" || -z "$QUERY_GTF" || -z "$TARGET_FA" || -z "$TARGET_GTF" || -z "$WORKDIR" ]]; then
+if [[ -z "$CONFIG" || -z "$WORKDIR" ]]; then
     echo "Error: Missing mandatory arguments"
     usage
 fi
 
 # Arguments used
-echo "Query genome: $QUERY_FA"
-echo "Query annotation: $QUERY_GTF"
-echo "Target genome: $TARGET_FA"
-echo "Target annotation: $TARGET_GTF"
+echo "Configuration file: $CONFIG"
 echo "Path to results directory: $WORKDIR"
+echo "Gene biotype to align: $BIOTYPE"
 echo "Flank value for liftoff: $FLANK"
 echo "Coverage cut-off for liftoff: $COVERAGE"
 echo "Sequence identity cut-off for liftoff: $IDENTITY"
 
-SPECIES1=$(basename $QUERY_GTF .gtf)
-SPECIES2=$(basename $TARGET_GTF .gtf)
+echo "Sequence alignment analysis - IN PROGRESS"
 
-### STEP1 - liftoff
-RESULTS="$WORKDIR"/work/method3/"$SPECIES1"_to_"$SPECIES2"
-mkdir -p ${RESULTS}
+# read config file to retrieve each species pair
+declare -A completeName shortName pathToGTF pathToFasta
 
-echo "Step1: liftoff analysis - IN PROGRESS"
-echo "Working directory: $WORKDIR"
+while IFS=',' read -r name short gtf fasta; do
+    completeName["$name"]="$name"
+    shortName["$name"]="$short"
+    pathToGTF["$name"]="$gtf"
+    pathToFasta["$name"]="$fasta"
+done < <(tail -n +2 "$CONFIG") 
 
-# perform sequence alignment according BIOTYPE argument
-echo "Gene biotype to analyze: $BIOTYPE"
-case "$BIOTYPE" in
-    all)
-        ## select features to add to `-f`
-        FEATURES=$RESULTS/all_features.txt
-        grep -v '#' $QUERY_GTF | cut -f3 | sort | uniq > $FEATURES
-        echo "Features file created: $FEATURES"
+# list of species to analyse
+species=("${!completeName[@]}")
 
-        ## create outfile and outdir
-        LIFTOFF_DIR="$RESULTS"/liftoff_flank"$FLANK"
-        LO_OUT=liftoff_"$SPECIES1"_to_"$SPECIES2"_flank"$FLANK".gtf
+# generate all species pair to analyse
+for ((i = 0; i < ${#species[@]}; i++)); do
+    for ((j = 0; j < ${#species[@]}; j++)); do
+        if [[ $i -ne $j ]]; then
+            species1="${species[$i]}"
+            species2="${species[$j]}"
 
-        ## launch liftoff script
-        echo "Launch liftoff analysis: "
-        ### source environment
-        . /local/env/envconda3.sh
-        conda activate activate /home/genouest/cnrs_umr6290/abesson/conda_env/liftoff_env
+            echo "Pair analysis : $species1 to $species2"
+            echo "Sequence alignment starts : $(date +"%d.%m.%Y %H:%M")"
 
-        ### run liftoff
-        echo "liftoff -g $QUERY_GTF -o $LO_OUT -p 8 $TARGET_FA $QUERY_FA -flank $FLANK -f $FEATURES -dir $LIFTOFF_DIR -u $LIFTOFF_DIR/unmapped_features.txt -copies"
-        liftoff -g $QUERY_GTF -o $LO_OUT -p 8 $TARGET_FA $QUERY_FA -flank $FLANK -f $FEATURES -dir $LIFTOFF_DIR -u $LIFTOFF_DIR/unmapped_features.txt -copies
+            # result files/directories
+            ## directory results by species
+            RESULTS="$WORKDIR"/work/method3/"${shortName[$species1]}"
+            echo "mkdir -p $RESULTS"
 
-        ;;
-    lncRNA)
-        echo "option not available yet"
-        ;;
-    mRNA)
-        echo "option not available yet"
-        ;;
-esac
+            ## files: subset annotation by biotype & feature database associated
+            file_name=$(basename ${pathToGTF[$species1]} .gtf)_"$BIOTYPE".gtf
+            QUERYDB="$RESULTS"/"$file_name"_db
 
-# move features file in liftoff dir
-mv $FEATURES $LIFTOFF_DIR
-mv $LO_OUT $RESULTS
-
-LO_OUT2="$RESULTS"/"$LO_OUT"
-
-### STEP2 - Filter liftoff results according cut-off arguments
-echo "Step2: liftoff results filtering - IN PROGRESS"
-
-# filtered file
-LO_FILTER="$RESULTS"/$(basename $LO_OUT2 .gtf)_filtered.gtf
-
-conda deactivate
-conda activate /home/genouest/cnrs_umr6290/abesson/conda_env/jupyterR_env
-
-Rscript filter_liftoffOutput.R $LO_OUT2 $LO_FILTER $COVERAGE $IDENTITY $RESULTS
-
-### STEP3 - Bedtools intersect
-echo "Step3: bedtools analysis - IN PROGRESS"
-BEDTOOLS_DIR="$RESULTS"/bedtools_intersect
-mkdir ${BEDTOOLS_DIR}
-
-### create outfile for liftoff output, target annotation and bedtools output
-LO_BED="$BEDTOOLS_DIR"/$(basename $LO_FILTER .gtf).bed
-TARGET_BED="$BEDTOOLS_DIR"/"$SPECIES2".bed
-FINAL_BED="$BEDTOOLS_DIR"/overlap_"$SPECIES1"_to_"$SPECIES2".bed
-
-#### format gtf in bed 
-. format_gtf2bed.sh $LO_FILTER > $LO_BED
-echo "Bed file created: $LO_BED"
-
-. format_gtf2bed.sh $TARGET_GTF > $TARGET_BED
-echo "Bed file created: $TARGET_BED"
-
-#### bedtools intersect - default fraction ~1bp
-conda deactivate
-. /local/env/envbedtools-2.27.1.sh
-
-echo "Intersection analysis done by bedtools intersect."
-bedtools intersect -a $LO_BED -b $TARGET_BED -wao -split > $FINAL_BED
-echo "bedtools intersect -a $LO_BED -b $TARGET_BED -wao -split > $FINAL_BED"
-
-### STEP4 - Sequence alignment analysis
-echo "Step4: Sequence alignment analysis - IN PROGRESS"
-ALIGN_DIR="$RESULTS"/alignment_analysis
-mkdir ${ALIGN_DIR}
-
-# load conda env => en créer un spécifiquement pour ça ??
-conda deactivate
-conda activate /home/genouest/cnrs_umr6290/abesson/conda_env/jupyterR_env
-
-echo "Rscript seq_alignment_analysis.R $QUERY_GTF $TARGET_GTF $FINAL_BED $LIFTOFF_DIR/unmapped_features.txt $ALIGN_DIR"
-Rscript seq_alignment_analysis.R $QUERY_GTF $TARGET_GTF $FINAL_BED $LIFTOFF_DIR/unmapped_features.txt $ALIGN_DIR
+            ## sub-directory for each pair analysis
+            COMPDIR="$RESULTS"/"${shortName[$species1]}"_to_"${shortName[$species2]}"_"$BIOTYPE"
+            
+            # perform sequence aligment by pair
+            ## check if output dir already exists
+            if [ ! -d "$COMPDIR" ]; then
+                ## check if feature database already exists
+                if [ -f $QUERYDB ]; then
+                    echo "Features database already exists: $QUERYDB."
+                    echo "Liftoff process starts."
+                    bash perform_liftoff_byPair.sh \
+                        --query_sp ${shortName[$species1]} \
+                        --query_fa ${pathToFasta[$species1]} \
+                        --query_gtf ${pathToGTF[$species1]} \
+                        --target_sp ${shortName[$species2]}\
+                        --target_fa ${pathToFasta[$species2]} \
+                        --target_gtf ${pathToGTF[$species2]} \
+                        --workdir $RESULTS \
+                        --biotype $BIOTYPE \
+                        --flank $FLANK \
+                        --coverage $COVERAGE \
+                        --identity $IDENTITY \
+                        --withDB TRUE
+                    echo "Sequence alignment ends : $(date +"%d.%m.%Y %H:%M")"
+                    echo " "
+                else
+                    echo "Features database doesn't exist."
+                    echo "$QUERYDB"
+                    echo "Liftoff process starts."
+                    bash perform_liftoff_byPair.sh \
+                        --query_sp ${shortName[$species1]} \
+                        --query_fa ${pathToFasta[$species1]} \
+                        --query_gtf ${pathToGTF[$species1]} \
+                        --target_sp ${shortName[$species2]}\
+                        --target_fa ${pathToFasta[$species2]} \
+                        --target_gtf ${pathToGTF[$species2]} \
+                        --workdir $RESULTS \
+                        --biotype $BIOTYPE \
+                        --flank $FLANK \
+                        --coverage $COVERAGE \
+                        --identity $IDENTITY \
+                        --withDB FALSE
+                    echo "Sequence alignment ends : $(date +"%d.%m.%Y %H:%M")"
+                    echo " "
+                fi
+            else
+                echo "Directory $COMPDIR already exists. Sequence analysis not performed."
+                echo " "
+            fi  
+        fi
+    done
+done
+echo "All sequence alignments done."
+echo "$(date +"%d.%m.%Y %H:%M")"
